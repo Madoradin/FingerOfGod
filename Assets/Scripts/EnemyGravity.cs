02e0c2978fc018cbe38db11f09e9f78e8c9a7361
Owen Lin <lin.owen@gmail.com>
2023-02-26 20:11:20 -0800

    Fixed animatioons to use local position instead of world position

--- Assets/Scripts/EnemyGravity.cs
+++ Assets/Scripts/EnemyGravity.cs
@@ -6,32 +6,42 @@ using UnityEngine.AI;
 public class EnemyGravity : MonoBehaviour
 {
     private bool isDead;
+    private bool isLifted;
     [SerializeField] private float maxHeight = 7f;
+    [SerializeField] private Animator animator;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        isLifted = false;
         isDead = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.y > maxHeight)
+        if (transform.position.y > 0)
         {
-            isDead = true;
+            isLifted = true;
+
+            if (transform.position.y > maxHeight)
+            {
+                isDead = true;
+            }
         }
+        animator.SetBool("Lifted", isLifted);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.tag == "Ground" && isDead == true)
+        if(collision.gameObject.CompareTag("Ground") && isDead == true)
         {
             gameObject.GetComponent<Enemy>().DestroyObject();
             Destroy(gameObject);
         }
 
+        isLifted = false;
         gameObject.GetComponent<EnemyNav>().ReEnableNav();
     }
 }
